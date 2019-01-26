857f41341d6192c656d8dc3e7e7585510fe51f44
Jacob Sipple <sipple3@gmail.com>
2019-01-26 17:51:49 -0500

    changed the size of the chart and fixed issue with some of the values not loading in time

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let API = "https://api.iextrading.com/1.0"
 let query = "/stock/aapl/chart"
 let date = []
-let yAxesMin;
-let yAxesMax;
+// let yAxesMin;
+// let yAxesMax;
 let stockValue = []
 function getURL() {
 url = API + query
@@ -10,20 +10,20 @@ $.get(url).then(function(obj) {
     for (let i = 0; i < 10; i++) {
     date.push(obj[i].date)
     stockValue.push(obj[i].vwap)
-    yAxesMin = Math.min(...stockValue) - 10
-    console.log(yAxesMin)
+    // yAxesMin = Math.min(...stockValue) - 10
     console.log(stockValue)
     }
 }).catch( error => console.log(error))
-}
-getURL()    
+} 
+getURL()
+setTimeout(function() {
 var ctx = document.getElementById("myChart").getContext('2d');
-var stockChart = new Chart(ctx, {
+var myChart = new Chart(ctx, {
     type: 'line',
     data: {
         labels: date,
         datasets: [{
-            label: '# of Votes',
+            label: 'stock price',
             data: stockValue,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)'
@@ -35,11 +35,10 @@ var stockChart = new Chart(ctx, {
         }]
     },
     options: {
-        resposive:true,
+        responsive:true,
         maintainAspectRatio:false,
         scales: {
             yAxes: [{
-
                 ticks: {
                     beginAtZero:false
                 }
@@ -47,3 +46,4 @@ var stockChart = new Chart(ctx, {
         }
     }
 });
+},1000)
