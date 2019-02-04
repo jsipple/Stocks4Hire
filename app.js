fe6a1a9fbafe2e40f04cd483f16b4ee2755291d2
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2019-02-04 10:57:32 -0500

    add timer and news links

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ $(document).ready(function() {
 
     let stockValue = []
 
-    var myChart;
-
     var clicked = true;
 
     // Initialize Firebase
@@ -76,7 +74,7 @@ $(document).ready(function() {
         var close = $("<td>").text("$" + response[input].chart[20].close);
         var canvas = $("<canvas>");
 
-        canvas.attr("id", input);
+        canvas.attr("id", input).hide();
 
         var table = $("<tr>").append(name, close, "<br>").attr("val", input).addClass("chart");
 
@@ -117,19 +115,180 @@ $(document).ready(function() {
                             beginAtZero:false
                         }
                     }]
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDfQP6TQSTiLKBpE16fqOd_JDx-xfCS55g",
-    authDomain: "stocks-eeae4.firebaseapp.com",
-    databaseURL: "https://stocks-eeae4.firebaseio.com",
-    projectId: "stocks-eeae4",
-    storageBucket: "stocks-eeae4.appspot.com",
-    messagingSenderId: "623424739833"
-  };
-  firebase.initializeApp(config);
+                }
+            }
+        });
+        $(document).unbind().on("click", ".chart", function(){
 
-let database = firebase.database();
-let auth = firebase.auth()
+            console.log("hey")
+            
+            var click = $(this).attr("val");
+    
+            if(!clicked){
+    
+                $("#" + click).hide();
+    
+                console.log(clicked);
+                clicked = true;
+
+               
+            }
+            else if(clicked){
+     
+                $("#" + click).show();
+
+                console.log(clicked);
+                clicked = false;
+
+                
+            }
+        });
+    
+    function getNews(item){
+    
+        var URL = 'https://newsapi.org/v2/everything?q=' + item + 's&apiKey=d53b18e6f2bb4408bb4b79dd3dfb406b'
+    
+        $.ajax({
+            url: URL,
+            method: "GET"
+            })
+            .then(function(response){
+            
+            for(i = 0; i < 5; i++){
+                var newsURL = response.articles[i].url;
+                var title = response.articles[i].title;
+                var date = response.articles[i].publishedAt.substr(0,10);
+                var author = response.articles[i].author;
+                
+                var itemval=item.replace(/[^a-zA-Z ]/g, "").split(" ").join("");
+                
+                var tbody = $("#newsArticles");
+                var name = $("<td>").text('Click here for latest news for '+item);
+                var newslink = $("<td>").html('<a href="'+newsURL+'" target="blank">'+title+' (Date: '+date+') '+'Author: '+author+'</a>');
+                var mainrow = $("<tr>").append(name, "<br>").attr("val", itemval).addClass("newslinks");
+                
+                var table = $("<tr class="+itemval+">").append(newslink, "<br>");
+                if(i==0){
+                    tbody.append(mainrow);
+                }
+                tbody.append(table);
+                $('.'+itemval).hide();                    
+                $(".newslinks").unbind().on("click", function(){ 
+                    var click1 = "."+$(this).attr("val");
+                    console.log('click1 '+click1);
+                    
+                    if(!clicked){
+                        $(click1).hide();
+                        clicked = true;
+                    }else if(clicked){
+                        $(click1).show();
+                        clicked = false;
+                    }
+                });            
+            };
+            
+        });
+    }
+
+    });
+
+    });
+
+
+    ////Market close/open TIMER
+    var tday =moment('15:30', 'HH:mm');
+    var minAway=tday.diff(moment(),"s");
+    var secAway=minAway*1000;
+    var marketStatus="Time Until market closes: ";
+
+    var pastMidNight=parseInt((moment().format('HH')));
+    var pastMidNightM=parseInt((moment().format('mm')));
+    
+    //accounting for the midnight time 
+    if((pastMidNight<9) || ((pastMidNight==9) && pastMidNightM<=30) ){
+        minAway=moment().diff(tday,"s");
+    }
+    
+    if(minAway<0){
+        var nextDay=moment().add(1, 'd').format('MM-DD-YYYY');
+        var open=moment(nextDay+'9:30', 'MM-DD-YYYY HH:mm')
+        var minAway=open.diff(moment(),"s");
+        var secAway=minAway*1000;
+        var marketStatus="Time Until market opens: ";
+    }
+
+    //Accounting for weekend
+    function dayFinder(dayINeed){
+        // if we haven't yet passed the day of the week that I need:
+        if (moment().isoWeekday() <= dayINeed) { 
+            // then just give me this week's instance of that day
+            var dayRequested=moment().isoWeekday(dayINeed);
+            return dayRequested;
+        } else {
+            // otherwise, give me next week's instance of that day
+            var dayRequested=moment().add(1, 'weeks').isoWeekday(dayINeed);
+            return dayRequested;
+        }
+    }
+        
+    var monday=dayFinder(1);
+ 
+    var weekday=moment().weekday();
+    
+    if( //after 3:30 on friday
+        ((weekday>5) || (weekday==5 && (parseInt(moment().format('HH')==13) && parseInt(moment().format('mm')>30) || parseInt(moment().format('HH')>=14))) || (weekday==0)) 
+            ||  
+        (   (weekday==1) && ((parseInt(moment().format('HH'))<9) || (parseInt(moment().format('HH'))==9 && parseInt(moment().format('mm'))<25))
+        )
+    ){  //check if the current date is less than monday
+        $("#marketTimer").html("Market is closed on weekend Reopens on "+monday.format('MM/DD/YYYY')+" @ 9:30 a.m"); 
+    }
+    else {//show timer
+ 
+    var countDownQ =new Date().getTime()+secAway;
+    var y = setInterval(function() {
+    var nowQ = new Date().getTime();
+    var distanceQ = countDownQ - nowQ;
+    var daysQ = Math.floor(distanceQ / (1000 * 60 * 60 * 24));
+    var hoursQ = Math.floor((distanceQ % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutesQ = Math.floor((distanceQ % (1000 * 60 * 60)) / (1000 * 60));
+    var secondsQ = Math.floor((distanceQ % (1000 * 60)) / 1000);
+    if (minutesQ<9){
+        var delim=" : 0";
+    }else{
+        var delim=" : "; 
+    }
+    if (hoursQ<9){
+        var hdelim=" 0";
+    }else{
+        var hdelim=" "; 
+    }
+
+        if(secondsQ>=0){$("#marketTimer").html("Financial News: &nbsp &nbsp &nbsp  &nbsp &nbsp"+marketStatus+hdelim+hoursQ+' Hours '+delim+minutesQ+' Minutes'); }
+        
+        if (distanceQ < 0 ) {
+            clearInterval(y);
+        }
+    }, 1000);
+
+}
+    //END OF TIMER
+
+
+
+// Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyDfQP6TQSTiLKBpE16fqOd_JDx-xfCS55g",
+//     authDomain: "stocks-eeae4.firebaseapp.com",
+//     databaseURL: "https://stocks-eeae4.firebaseio.com",
+//     projectId: "stocks-eeae4",
+//     storageBucket: "stocks-eeae4.appspot.com",
+//     messagingSenderId: "623424739833"
+//   };
+//   firebase.initializeApp(config);
+
+// let database = firebase.database();
+// let auth = firebase.auth()
 // $("#signIn").on("click", function(event) {
 //     event.preventDefault()
 //     const email = $("#email").val().trim()
@@ -137,134 +296,25 @@ let auth = firebase.auth()
 //     const signIn = auth.signInWithEmailAndPassword(email,pass)
 //     signIn.catch(e => console.log(e.message))
 // })
-console.log('running')
-$("#signUp").on("click", function(event) {
-    console.log("test")
-    event.preventDefault();
-    email = $("#email").val().trim()
-    pass = $("#password").val().trim()
-    console.log("email")
-    const signUp = auth.createUserWithEmailAndPassword(email,pass)
-    signUp.catch(e => console.log(e.message))
-})
+// console.log('running')
+// $("#signUp").on("click", function(event) {
+//     console.log("test")
+//     event.preventDefault();
+//     email = $("#email").val().trim()
+//     pass = $("#password").val().trim()
+//     console.log("email")
+//     const signUp = auth.createUserWithEmailAndPassword(email,pass)
+//     signUp.catch(e => console.log(e.message))
+// })
 
-auth.onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-        console.log(`user logged in`)
-    } else {
-        console.log(`user logged out`)
-    }
-})
+// auth.onAuthStateChanged(firebaseUser => {
+//     if (firebaseUser) {
+//         console.log(`user logged in`)
+//     } else {
+//         console.log(`user logged out`)
+//     }
+// })
 
-
-let API = "https://api.iextrading.com/1.0"
-let query = "/stock/aapl/chart"
-let date = 5
-// let yAxesMin;
-// let yAxesMax;
-let stockValue = []
-function getURL() {
-url = API + query
-$.get(url).then(function(obj) {
-
-    console.log(obj);
-
-    for (let i = 0; i < 20; i++) {
-    date.push(obj[i].date)
-    stockValue.push(obj[i].vwap)
-    // yAxesMin = Math.min(...stockValue) - 10
-    console.log(stockValue)
-    }
-}).catch( error => console.log(error))
-} 
-getURL()
-// chart need to make it so that it changes based on different click functions
-setTimeout(function() {
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: date,
-        datasets: [{
-            label: 'stock price',
-            data: stockValue,
-            backgroundColor: [
-                'rgba(0, 0, 0, 0.5)'
-            ],
-            borderColor: [
-                'rgba(0, 255, 0, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive:true,
-        maintainAspectRatio:true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:false
-                }
-            }
-    
-            });
-        }, 1);
-
-        
-            var click = $(this).attr("val");
-
-            $("#" + click).hide();
-
-            console.log(click);
-        });
-
-        console.log("Made chart");
-
-    $(document).on("click", ".chart", function(){
-
-        console.log("hey")
-        
-        var click = $(this).attr("val");
-
-        if(!clicked){
-
-            $("#" + click).show();
-
-            console.log(clicked);
-
-            clicked = true;
-        }
-        else if(clicked){
-
-            $("#" + click).hide();
-            
-            clicked = false;
-        }
-    });
-
-function getNews(item){
-
-    var URL = 'https://newsapi.org/v2/everything?q=' + item + 's&apiKey=d53b18e6f2bb4408bb4b79dd3dfb406b'
-
-    $.ajax({
-        url: URL,
-        method: "GET"
-      })
-      .then(function(response){
-        
-        for(i = 0; i < 5; i++){
-            var newsURL = response.articles[i].url;
-            var title = response.articles[i].title;
-            var date = response.articles[i].publishedAt.substr(0,10);
-            var author = response.articles[i].author;
-            
-            $("#news" + i).append('<a href="'+newsURL+'" target="blank">'+title+' (Date: '+date+') '+'Author: '+author+'</a><br>');
-        
-      };
-    });
-}
-
-});
 // when you click on a stock it dropsdown below(using bootstrap) and shows a graph with one month of stock data
 
 // when clicking the star icon next to the stock it will add it to the users favorites firebase
@@ -285,3 +335,4 @@ function getNews(item){
 
 // add buttons above graph that will change timeframe
 
+});
